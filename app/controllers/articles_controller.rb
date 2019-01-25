83bd95d81e244cd1359976778c126ad67e4495fb
booty <johnedmundrose@gmail.com>
2019-01-25 13:07:56 -0500

    List newest articles first, improve article styling

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all.newest_first
   end
 
   def new
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,8 @@
 class Article < ApplicationRecord
   validates :body, presence: true
   validates :title, presence: true
+
+  def self.newest_first
+    order(id: :desc)
+  end
 end
